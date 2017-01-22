1e5926576afc5a8cfbb3bbbb51785a1cae03d195
ThomasCarpaneto95 <ThomasCarpaneto@gmail.com>
2017-01-21 19:32:22 -0700

    dfgh

--- Clippr/ViewController.swift
+++ Clippr/ViewController.swift
@@ -10,13 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var cliptitle: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
+        
         // Dispose of any resources that can be recreated.
     }
 
