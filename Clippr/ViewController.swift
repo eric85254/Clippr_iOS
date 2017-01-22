15c967a1dfa3c30278668c99a33bdf052c9ecee8
ThomasCarpaneto95 <ThomasCarpaneto@gmail.com>
2017-01-21 20:28:37 -0700

    yes

--- Clippr/ViewController.swift
+++ Clippr/ViewController.swift
@@ -12,12 +12,16 @@ class ViewController: UIViewController {
 
     
     
+    @IBOutlet weak var clipprLogo: UIImageView!
+    
+    @IBOutlet weak var signUP: UIButton!
     @IBOutlet weak var signIn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.signIn.layer.borderWidth = 2
         self.signIn.layer.borderColor = UIColor.blue.cgColor
+        self.signUP.layer.borderColor = UIColor.blue.cgColor
         // Do any additional setup after loading the view, typically from a nib.
     }
 
